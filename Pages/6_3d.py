2aac923ba736da6c834d722d0d9e813150065a84
sakunici <sakunichm@gmail.com>
2025-05-18 23:42:02 +0700

    feature: add 3d

---
+++ Pages/6_3d.py
@@ -0,0 +1,32 @@
+import random
+from streamlit_d3_demo import d3_line
+import streamlit as st
+
+def generate_random_data(x_r, y_r):
+    return list(zip(range(x_r), [random.randint(0, y_r) for _ in range(x_r)]))
+
+st.title("D3 Demo Line Chart")
+st.write("This is a demo of D3 line chart")
+
+# Add form for user input
+with st.form("chart_params"):
+    col1, col2 = st.columns(2)
+    
+    with col1:
+        x_range = st.number_input("Number of points (X-range)", 
+                                min_value=5, 
+                                max_value=50, 
+                                value=20)
+    
+    with col2:
+        y_range = st.number_input("Maximum Y value", 
+                                min_value=100, 
+                                max_value=1000, 
+                                value=500)
+    
+    submit_btn = st.form_submit_button("Generate Chart")
+
+# Generate and display chart when form is submitted
+if submit_btn:
+    data = generate_random_data(x_range, y_range)
+    d3_line(data, circle_radius=15, circle_color="#ff6f91")
